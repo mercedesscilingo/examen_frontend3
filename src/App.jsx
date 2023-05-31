e6ddf99a527a27707ac4b551571d8b0559bf73ba
mercedesscilingo <mercedes.scilingo@gmail.com>
2023-05-31 15:30:53 -0300

    firs commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Examen Front-End 3</h1>
+        <form></form>
+        <Card/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+
+const Card = () => {
+    return(
+        <div className="card">Hola , soy una tarjeta</div>
+    )
+};
+    
+export default Card;
